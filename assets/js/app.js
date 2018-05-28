d7a61f2bc6ad5e4fca5ac7cb23d5a1252cbe3c8c
MoyArt <moyart@gmail.com>
2018-05-28 16:47:32 -0500

    Comments added to JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,17 @@
 // Global Variables
+/* 
+    We use canvas to all concerns about screen size. & we use Canvas Context to all graphical information.
+*/
 let canvas,
 canvasContext,
 ballX = 50,
 ballSpeedX = 10,
 framesPerSecond = 30,
 ballSpeed = 1000/framesPerSecond;
-/* 
-We use canvas to all concerns about screen size. & we use Canvas Context to all graphical information.
-*/
 
+/* 
+    our onload function.
+*/
 window.onload = function(){
     console.log('Window finished loading, App scripts loaded');
     canvas = document.getElementById('gameCanvas');
@@ -20,6 +23,9 @@ window.onload = function(){
     
 }
 
+/*
+    This function hel us to move everything on the canvas
+*/
 function moveEverithing(){
     ballX = ballX + ballSpeedX;
     if(ballX<0){
@@ -30,6 +36,9 @@ function moveEverithing(){
     };
 }
 
+/*
+    This function draws all the elements on the canvas
+*/
 function drawEverithing(){   
     /* First we draw our background*/
     drawElement(0, 0, canvas.width, canvas.height, 'black');
@@ -39,12 +48,19 @@ function drawEverithing(){
     drawCircle(ballX, 100, 10, 'lime');
 }
 
+/*
+    This function help us to create new round elements to my canvas
+*/
 function drawCircle(centerX, centerY, radius, fillColor){
     canvasContext.fillStyle = fillColor;
     canvasContext.beginPath();
     canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true);
     canvasContext.fill();
 }
+
+/*
+    This function help us to create new box elements to my canvas
+*/
 function drawElement(topX, topY, canvasWidth, canvasHeight, fillColor){
     canvasContext.fillStyle = fillColor;
     canvasContext.fillRect(topX, topY, canvasWidth, canvasHeight);
