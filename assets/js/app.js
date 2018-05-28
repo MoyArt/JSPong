5b3107efa384139e7d144500ee511678262a61a0
MoyArt <moyart@gmail.com>
2018-05-28 10:31:08 -0500

    Added Draw function to draw our game on canvas, and added moveEverithing function to animate our elements

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 // Global Variables
 let canvas,
-canvasContext;
+canvasContext,
+ballX = 50,
+framesPerSecond = 30,
+ballSpeed = 1000/framesPerSecond;
 /* 
 We use canvas to all concerns about screen size. & we use Canvas Context to all graphical information.
 */
@@ -9,7 +12,32 @@ window.onload = function(){
     console.log('Window finished loading, App scripts loaded');
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
+    setInterval(function(){
+        moveEverithing();
+        drawEverithing();
+    }, ballSpeed)
+    
+}
+
+function moveEverithing(){
+    ballX = ballX + 10;
+    if(ballX === 800){
+        ballX = 0;
+        ballSpeed = ballSpeed + 10;
+    }
+    console.log(ballX);
+    console.log(ballSpeed);
+}
+
+function drawEverithing(){   
+    /* First we draw our background*/
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    /* Then we draw our paddle */
+    canvasContext.fillStyle = 'white';
+    canvasContext.fillRect(1, 250, 15, 100);
+    /* Finally we draw our ball */
+    canvasContext.fillStyle = 'lime';
+    canvasContext.fillRect(ballX, 200, 10, 10);
 }
 
