1d467e32af86281c5691f1c58a2bef366a2cc5ef
MoyArt <moyart@gmail.com>
2018-05-28 09:42:11 -0500

    Added styles, added canvas element and context

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,15 @@
-console.log('App scripts loaded')
+// Global Variables
+let canvas,
+canvasContext;
+/* 
+We use canvas to all concerns about screen size. & we use Canvas Context to all graphical information.
+*/
+
+window.onload = function(){
+    console.log('Window finished loading, App scripts loaded');
+    canvas = document.getElementById('gameCanvas');
+    canvasContext = canvas.getContext('2d');
+    canvasContext.fillStyle = 'black';
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+}
+
